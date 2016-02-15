bca3c299461b3cdd06adaee8675bfb33fb6282f7
Brandon Him <brh55@drexel.edu>
2016-02-14 22:35:27 -0600

    Set up visualization sub-navigation

---
+++ src/app/components/navigation/exploreNavigation.directive.js
@@ -0,0 +1,17 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('digglyFeProto')
+        .directive('exploreNavigation', exploreNavigation);
+
+    /** @ngInject */
+    function exploreNavigation () {
+        var directive = {
+            restrict: 'EA',
+            templateUrl: 'app/components/navigation/nav.html'
+        }
+
+        return directive;
+    }
+})();
