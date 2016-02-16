2686ee68896b4c0f60309a1ea392d67235af0d8e
Brandon Him <brh55@drexel.edu>
2016-02-16 18:43:31 -0500

    Create clear history, remove item clicks

--- src/app/explore/controllers/history.controller.js
+++ src/app/explore/controllers/history.controller.js
@@ -1,37 +1,40 @@
 (function() {
   'use strict';
 
-  angular
-    .module('digglyFeProto')
-    .controller('HistoryController', HistoryController);
+    angular
+        .module('digglyFeProto')
+        .controller('HistoryController', HistoryController);
 
-  /** @ngInject */
-  function HistoryController(ExploreService) {
-    var m = this.model = {
-        history: [],
-    };
+    /** @ngInject */
+    function HistoryController(ExploreService) {
+        var m = this.model = {
+            history: [],
+        };
 
-    var a = this.action = {
-        /**
-         * Wrapper to remove current item
-         * @param  {object} currentItem current history item to be remove
-         * @return {void}
-         */
-        removeItem: function(currentItem) {
-          ExploreService.removeBookmarkItem(currentItem);
-        },
-        /**
-         * Wrapper to clear history
-         * @return {void}
-         */
-        clearHistory: function() {
-          ExploreService.clearHistory();
-        },
-        init: function() {
-            m.history = ExploreService.getHistory() || '';
-        }
-    };
+        var a = this.action = {
+            /**
+             * Wrapper to remove current item
+             * @param  {object} currentItem current history item to be remove
+             * @return {void}
+             */
+            removeItem: function(currentItem) {
+                ExploreService.removeBookmarkItem(currentItem);
+            },
 
-    a.init();
-  }
+            /**
+             * Wrapper to clear history
+             * @return {void}
+             */
+            clearHistory: function() {
+                ExploreService.clearHistory();
+                m.history.length = 0;
+            },
+
+            init: function() {
+                m.history = ExploreService.getHistory() || '';
+            }
+        };
+
+        a.init();
+    }
 })();
