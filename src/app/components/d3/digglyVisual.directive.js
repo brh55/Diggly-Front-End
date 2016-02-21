467ef12a6639e6218aed5351ddd6f5fd13aa202c
Brandon Him <brh55@drexel.edu>
2016-02-21 01:16:09 -0500

    Fix D3 Visualizer issue

--- src/app/components/d3/digglyVisual.directive.js
+++ src/app/components/d3/digglyVisual.directive.js
@@ -54,157 +54,175 @@
               // When nothing exist, return
               if (!data) return;
 
-
               // Set up Data
               m.d3Data.nodes = createNodeData(data);
               m.d3Data.edges = createEdges(data.linked_topics.length);
 
-              var colors = d3.scale.category10();
+              var colors = d3
+                .scale
+                .category10();
 
-              var force = d3.layout.force()
-                            .nodes(m.d3Data.nodes)
-                            .links(m.d3Data.edges)
-                            .size([500, 500])
-                            .linkDistance(130)
-                            .charge([-500])
-                            .theta(0.1)
-                            .gravity(0.05)
-                            .on('tick', tick)
-                            .start();
+              var force = d3
+                .layout
+                .force()
+                .nodes(m.d3Data.nodes)
+                .links(m.d3Data.edges)
+                .size([500, 500])
+                .linkDistance(130)
+                .charge([-500])
+                .theta(0.1)
+                .gravity(0.05)
+                .on('tick', tick)
+                .start();
 
-             var edges = svg.selectAll('line')
-                            .data(m.d3Data.edges)
-                            .enter()
-                            .append('line')
-                            .attr('id',function(d,i) { return 'edge' + i })
-                            .attr('marker-end','url(#marker_circle)')
-                            .style('stroke','#ccc')
-                            .style('pointer-events', 'none');
+              var edges = svg
+                .selectAll('line')
+                .data(m.d3Data.edges)
+                .enter()
+                .append('line')
+                .attr('id',function(d,i) { return 'edge' + i })
+                .attr('marker-end','url(#marker_circle)')
+                .style('stroke','#ccc')
+                .style('pointer-events', 'none');
 
-              var nodes = svg.selectAll('circle')
-                            .data(m.d3Data.nodes)
-                            .enter()
-                            .append('circle')
-                            .attr({
-                              'r': function(d, i) {
-                                  return d.score * 40 // TODO: Find a better number or dynamic number based on sizing
-                              },
-                              'class': function(d, i) {
-                                  return 'node-' + i;
-                              }
-                            })
-                            .style('fill', function(d, i) {
-                              return colors(i);
-                            })
-                            .style('cursor', function(d, i) {
-                              return (i === 0) ? 'move' : 'pointer';
-                            })
-                            .call(force.drag)
-                            .on('click', function(d, i) {
-                              if (i !== 0) {
-                                var selectedId = d.target_id;
-                                scope.onClick({item: selectedId})
-                              }
-                            });
+              var nodes = svg
+                .selectAll('circle')
+                .data(m.d3Data.nodes)
+                .enter()
+                .append('circle')
+                .attr({
+                  'r': function(d, i) {
+                      return d.score * 40 // TODO: Find a better number or dynamic number based on sizing
+                  },
+                  'class': function(d, i) {
+                      return 'node-' + i;
+                  }
+                })
+                .style('fill', function(d, i) {
+                  return colors(i);
+                })
+                .style('cursor', function(d, i) {
+                  return (i === 0) ? 'move' : 'pointer';
+                })
+                .call(force.drag)
+                .on('click', function(d, i) {
+                  if (i !== 0) {
+                    var selectedId = d.target_id;
+                    scope.onClick({item: selectedId})
+                  }
+                });
 
-              var nodelabels = svg.selectAll('.nodelabel')
-                                .data(m.d3Data.nodes)
-                                .enter()
-                                .append('text')
-                                .attr({
-                                  'x': function (d) {
-                                    return d.x;
-                                  },
-                                  'y': function (d) {
-                                    return d.y;
-                                  },
-                                  'class': function(d, i) {
-                                    return 'label-' + i;
-                                  },
-                                  'stroke': 'black'
-                                })
-                                .text(function(d) {
-                                  return d.title;
-                                })
+              var nodelabels = svg
+                .selectAll('.nodelabel')
+                .data(m.d3Data.nodes)
+                .enter()
+                .append('text')
+                .attr({
+                  'x': function (d) {
+                    return d.x;
+                  },
+                  'y': function (d) {
+                    return d.y;
+                  },
+                  'class': function(d, i) {
+                    return 'label-' + i;
+                  },
+                  'stroke': 'black'
+                })
+                .text(function(d) {
+                  return d.title;
+                });
 
-              var edgepaths = svg.selectAll('.edgepath')
-                                .data(m.d3Data.edges)
-                                .enter()
-                                .append('path')
-                                .attr({
-                                  'd': function(d) {
-                                    return 'M ' + d.source.x + ' '+ d.source.y+' L '+ d.target.x +' '+ d.target.y;
-                                  },
-                                 'class':'edgepath',
-                                 'fill-opacity':0,
-                                 'stroke-opacity':0,
-                                 'fill':'blue',
-                                 'stroke':'red',
-                                 'id': function(d, i) {
-                                    return 'edgePath-' + i;
-                                 }
-                                })
-                                .style('pointer-events', 'none');
+              var edgepaths = svg
+                .selectAll('.edgepath')
+                .data(m.d3Data.edges)
+                .enter()
+                .append('path')
+                .attr({
+                  'd': function(d) {
+                    return 'M ' + d.source.x + ' '+ d.source.y+' L '+ d.target.x +' '+ d.target.y;
+                  },
+                 'class':'edgepath',
+                 'fill-opacity':0,
+                 'stroke-opacity':0,
+                 'fill':'blue',
+                 'stroke':'red',
+                 'id': function(d, i) {
+                    return 'edgePath-' + i;
+                 }
+                })
+                .style('pointer-events', 'none');
 
-              var edgelabels = svg.selectAll('.edgelabel')
-                                  .data(m.d3Data.edges)
-                                  .enter()
-                                  .append('text')
-                                  .style("pointer-events", "none")
-                                  .attr({
-                                    'class':'edgelabel',
-                                    'id': function(d, i) {
-                                      return 'edgelabel-' + i;
-                                    },
-                                    'dx':80,
-                                    'dy':0,
-                                    'font-size':10,
-                                    'fill':'#aaa'
-                                  });
+              var edgelabels = svg
+                .selectAll('.edgelabel')
+                .data(m.d3Data.edges)
+                .enter()
+                .append('text')
+                .style("pointer-events", "none")
+                .attr({
+                  'class':'edgelabel',
+                  'id': function(d, i) {
+                    return 'edgelabel-' + i;
+                  },
+                  'dx':80,
+                  'dy':0,
+                  'font-size':10,
+                  'fill':'#aaa'
+                });
 
-              edgelabels.append('textPath')
-                        .attr('xlink:href',function(d,i) {return '#edgepath'+i})
-                        .style("pointer-events", "none")
+              edgelabels
+                .append('textPath')
+                .attr('xlink:href',function(d,i) {
+                  return '#edgepath'+i
+                })
+                .style("pointer-events", "none")
 
-              svg.append('defs')
-                  .append('marker')
+              svg
+                .append('defs')
+                .append('marker')
+                .attr({
+                  'id':'arrowhead',
+                  'viewBox':'-0 -5 10 10',
+                  'refX':25,
+                  'refY':0,
+                  //'markerUnits':'strokeWidth',
+                  'orient':'auto',
+                  'markerWidth':10,
+                  'markerHeight':10,
+                  'xoverflow':'visible'
+                })
+                .append('svg:path')
+                .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+                .attr('fill', '#ccc')
+                .attr('stroke','#ccc');
+
+              var tick = function () {
+                edges
                   .attr({
-                    'id':'arrowhead',
-                    'viewBox':'-0 -5 10 10',
-                    'refX':25,
-                    'refY':0,
-                    //'markerUnits':'strokeWidth',
-                    'orient':'auto',
-                    'markerWidth':10,
-                    'markerHeight':10,
-                    'xoverflow':'visible'
-                  })
-                  .append('svg:path')
-                  .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-                  .attr('fill', '#ccc')
-                  .attr('stroke','#ccc');
-
-              force.on("tick", function(){
-                  edges.attr({"x1": function(d){return d.source.x;},
-                              "y1": function(d){return d.source.y;},
-                              "x2": function(d){return d.target.x;},
-                              "y2": function(d){return d.target.y;}
+                    "x1": function(d){return d.source.x;},
+                    "y1": function(d){return d.source.y;},
+                    "x2": function(d){return d.target.x;},
+                    "y2": function(d){return d.target.y;}
                   });
 
-                  nodes.attr(
-                    {
-                      "cx": function (d) { return d.x },
-                      "cy": function (d) { return d.y }
-                    }
-                  );
+                nodes
+                  .attr({
+                    "cx": function (d) { return d.x },
+                    "cy": function (d) { return d.y }
+                  });
 
-                  nodelabels.attr("x", function(d) { return d.x; })
-                            .attr("y", function(d) { return d.y; });
+                nodelabels
+                  .attr("x", function(d) { return d.x; })
+                  .attr("y", function(d) { return d.y; });
 
-                  edgepaths.attr('d', function(d) { var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
-                                                     //console.log(d)
-                                                     return path});
-                });
+                edgepaths
+                  .attr('d', function(d) {
+                    var path='M '+d.source.x+' '+d.source.y+' L '+ d.target.x +' '+d.target.y;
+                    return path
+                  });
+              };
+
+              force.on("tick", tick)
 
               } // End of Scope Render
         });
