8aec06816bb3f1a02b9953d946f53afb8ac9e527
Brandon Him <brh55@drexel.edu>
2016-04-02 21:08:27 -0400

    Add export text function

--- src/app/explore/controllers/bookmark.controller.js
+++ src/app/explore/controllers/bookmark.controller.js
@@ -6,9 +6,21 @@
         .controller('BookmarkController', BookmarkController);
 
     /** @ngInject */
-    function BookmarkController(ExploreService) {
+    function BookmarkController(ExploreService, $window, $scope) {
         var m = this.model = {
             bookmarks: [],
+            exportType: "",
+            extensions: [
+                {
+                    label: "Text File (.txt)",
+                    type: "txt"
+                },
+                {
+                    label: "Spreadsheet (.xls)",
+                    type: "xls"
+                }
+            ],
+            downloadUrl: ""
         };
 
         var a = this.action = {
@@ -31,8 +43,52 @@
                 m.bookmark.length = 0;
             },
 
+            /**
+             * Trigger when a extension type is set
+             * @return {string} returns the URL to invoke a download
+             */
+            export: function() {
+                switch(m.exportType) {
+                    case "xls":
+                        m.downloadUrl = a.exportXls();
+                        break;
+
+                    default:
+                    case "text":
+                        m.downloadUrl = a.exportText("kekeke");
+                        break;
+                }
+            },
+
+            /**
+             * Exports a Text file by creating a text blob
+             * @param  {string} text The string in the text file
+             * @return {url}      the url to the text file created
+             */
+            exportText: function (text) {
+                var data = new Blob([text], {type: 'text/plain'});
+
+                // If we are replacing a previously generated file we need to
+                // manually revoke the object URL to avoid memory leaks.
+                if (textFile !== null) {
+                    $window.URL.revokeObjectURL(textFile);
+                }
+
+                var textFile = $window.URL.createObjectURL(data);
+
+                // returns a URL you can use as a href
+                return textFile;
+            },
+
+=
+            exportXls: function() {
+                return "not working yet";
+            },
+
             init: function() {
                 m.bookmarks = ExploreService.getBookmarks() || '';
+                // Default is text file
+                m.downloadUrl = a.exportText("text");
             }
         };
 
