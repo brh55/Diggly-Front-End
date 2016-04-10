a1da14f2bf14fb1b19d0b50d4610ba1e957cb6da
Brandon Him <brh55@drexel.edu>
2016-04-10 00:55:22 -0400

    Add bookmarking Parsing and exporting of CSV and Txt

--- src/app/explore/controllers/bookmark.controller.js
+++ src/app/explore/controllers/bookmark.controller.js
@@ -9,18 +9,19 @@
     function BookmarkController(ExploreService, $window, $scope) {
         var m = this.model = {
             bookmarks: [],
-            exportType: "",
+            exportType: "txt",
             extensions: [
                 {
                     label: "Text File (.txt)",
                     type: "txt"
                 },
                 {
-                    label: "Spreadsheet (.xls)",
-                    type: "xls"
+                    label: "Comma-Seperated Values (.csv)",
+                    type: "csv"
                 }
             ],
-            downloadUrl: ""
+            downloadUrl: "",
+            fileName: "MyBookmarks"
         };
 
         var a = this.action = {
@@ -47,47 +48,144 @@
              * @return {string} returns the URL to invoke a download
              */
             export: function() {
+                var bookmarks = a.normalize();
+
                 switch(m.exportType) {
-                    case "xls":
-                        m.downloadUrl = a.exportXls();
+                    case "csv":
+                        // create parsed JSON
+                        var parsedJSON = _.map(bookmarks, function (article) {
+                            var parsedObj =  _.mapValues(article, function(value) {
+                                // Iterate through all values and parse with a encoded quote to replaced during CSV
+                               return _(value).toString().replace(/\"/g, '%22');
+                            });
+                            return parsedObj;
+                        });
+                        var convertedCsv = a.convertoCsv(parsedJSON, "Diggly-CSV", true);
+                        m.downloadUrl = a.exportTo("csv", convertedCsv);
                         break;
 
                     default:
-                    case "text":
-                        var text = JSON.stringify($window.__bookmarks__);
-                        m.downloadUrl = a.exportText(text);
+                    case "txt":
+                        var parsedJSON = _.map(bookmarks, function (article) {
+                            var parsedObj =  _.mapValues(article, function(value) {
+                                // Iterate through all values, replace all backslash with encoding
+                                // to designate new line and replace all intended quotes and commas
+                                // with URL encoding
+                               return " " + _(value).toString().replace(/\"/g, '%22').replace(/,/g, '%2C') + "%5c";
+                            });
+                            return parsedObj;
+                        });
+                        var text = JSON.stringify(parsedJSON);
+
+                        var parseText = text
+                                            // Remove quotes and commas from JSON object string
+                                            .replace(/"/g, '')
+                                            .replace(/,/g, '')
+                                            // Reinsert intended quotes and commas
+                                            .replace(/\%22/g, '"')
+                                            .replace(/%2C/g, ',')
+                                            // Create line break and double line break for end of objects
+                                            .replace(/\%5c/g, '\n')
+                                            .replace(/}/g, '\n\n')
+                                            // Strip out the unneeded JSON-specific characters
+                                            .replace(/{/g, '')
+                                            .replace(/\[/g, '')
+                                            .replace(/\]/g, '');
+
+                        console.log(parseText);
+
+                        m.downloadUrl = a.exportTo("txt", parseText);
                         break;
                 }
             },
 
+            normalize: function () {
+                var normalizedBookmarks = _.map($window.__bookmarks__, function (article) {
+                    var tempObj = {
+                        "Wikipedia ID": article.article_id,
+                        "Wikipedia Title": article.article_title,
+                        "Wikipedia Link": article.wiki_link,
+                        "Description": article.description,
+                        "Short Summary": article.summary
+                    };
+
+                    return tempObj;
+                });
+
+                return normalizedBookmarks;
+            },
             /**
              * Exports a Text file by creating a text blob
              * @param  {string} text The string in the text file
              * @return {url}      the url to the text file created
              */
-            exportText: function (text) {
-                var data = new Blob([text], {type: 'text/plain'});
+            exportTo: function (type, data) {
+                var blob;
 
-                // If we are replacing a previously generated file we need to
-                // manually revoke the object URL to avoid memory leaks.
-                if (textFile !== null) {
-                    $window.URL.revokeObjectURL(textFile);
+                switch (type) {
+                    case "txt":
+                        blob = new Blob([data], {type: 'text/plain'});
+                        break;
+
+                    case "csv":
+                        blob = new Blob([data], {type: 'text/csv'});
+                        break;
                 }
 
-                var textFile = $window.URL.createObjectURL(data);
+                if (fileUrl !== null) {
+                    $window.URL.revokeObjectURL(data);
+                }
+
+                var fileUrl = $window.URL.createObjectURL(blob);
 
                 // returns a URL you can use as a href
-                return textFile;
+                return fileUrl;
             },
 
-            exportXls: function() {
-                return "not working yet";
+            // REF: http://stackoverflow.com/questions/8847766/how-to-convert-json-to-csv-format-and-store-in-a-variable
+            convertoCsv: function (JSONData, ReportTitle, ShowLabel) {
+                //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+                var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+                var CSV = '';
+                //This condition will generate the Label/Header
+                if (ShowLabel) {
+                    var row = "";
+
+                    //This loop will extract the label from 1st index of on array
+                    for (var index in arrData[0]) {
+                        //Now convert each value to string and comma-seprated
+                        row += index + ',';
+                    }
+                    row = row.slice(0, -1);
+                    //append Label row with line break
+                    CSV += row + '\r\n';
+                }
+
+                //1st loop is to extract each row
+                for (var i = 0; i < arrData.length; i++) {
+                    var row = "";
+                    // 2nd loop will extract each column and convert it in string comma-seprated
+                    // Need to add quotes to prevent being seperated by content commas.
+                    for (var index in arrData[i]) {
+                        row += '"' + arrData[i][index] + '",';
+                    }
+                    row.slice(0, row.length - 1);
+                    //add a line break after each row
+                    CSV += row + '\r\n';
+                }
+
+                CSV.replace(/\%22/g, '"');
+
+                if (CSV === '') {
+                    return "Error with exporting";
+                }
+
+                return CSV;
             },
 
             init: function() {
                 m.bookmarks = ExploreService.getBookmarks() || '';
-                // Default is text file
-                m.downloadUrl = a.exportText("text");
+                a.export();
             }
         };
 
