1e679cfcac860eb41bb152a2a1356625e86f05e7
Brandon Him <brh55@drexel.edu>
2016-04-11 11:48:34 -0400

    Updates to loader and add clear history

--- src/app/explore/controllers/visual.controller.js
+++ src/app/explore/controllers/visual.controller.js
@@ -72,6 +72,15 @@
             ExploreService.addBookmark(item);
         },
 
+        /**
+         * Wrapper for clear history service
+         * @return {void}
+         */
+        clearHistory: function () {
+            ExploreService.clearHistory();
+            m.history.length = 0;
+        },
+
         onClick: function(item) {
             a.updateHistory();
             a.fetchTopic(item);
@@ -86,6 +95,7 @@
 
             // Use services for scalability
             m.history = ExploreService.getHistory() || [];
+            console.log(m.history);
             // Initialize bookmark or get them;
             $window.__bookmarks__ = ExploreService.getBookmarks() || [];
         }
