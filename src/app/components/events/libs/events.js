858ec58048d48e469f71519286b739fac2cdf9d5
Brandon Him <brh55@drexel.edu>
2016-04-16 18:11:31 -0400

    Remove events

--- src/app/components/events/libs/events.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// D3 Post Event Handling
-console.log('listen to events');
-$("body").on("D3:Click", function(event, itemId) {
-    console.log('recieved d3 click');
-    console.log(event);
-    console.log("This is the article_id clicked: " + itemId);
-});
