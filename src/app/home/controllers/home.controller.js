67504d91aeccd189488176e89e4985b74016a71e
Brandon Him <brh55@drexel.edu>
2016-04-20 18:58:35 -0400

    Remove unnecessary watch

--- src/app/home/controllers/home.controller.js
+++ src/app/home/controllers/home.controller.js
@@ -12,36 +12,24 @@
             popular: {},
             trending: {},
             recent: [],
-            loadedCount: 0
         };
 
         var a = this.action = {
             init: function () {
                 DigglyService.getRandomTopic().then(function(response) {
                     m.random = response.plain();
-                    m.loadedCount++;
                 });
                 DigglyService.getPopularTopic().then(function(response) {
                     m.popular = response.plain();
-                    m.loadedCount++;
                 });
                 DigglyService.getRecentTopics().then(function(response) {
                     m.recent = response.plain();
-                    m.loadedCount++;
                 });
                 DigglyService.getTrendingTopic().then(function(response) {
                     m.trending = response.plain();
-                    m.loadedCount++;
                 });
             }
         };
-
-        $scope.$watch(
-            "home.model.loadedCount",
-            function handleFooChange( newValue, oldValue ) {
-                console.log( "vm.fooCount:", newValue );
-            }
-        );
 
         a.init();
     }
